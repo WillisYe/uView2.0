61e2b05bc4fefb123af5288d0212dcd0e2ca3379
yewei <ywbyl@qq.com>
2026-07-01 18:12:31 +0800

    feat: 色彩 按鈕

--- App.vue
+++ App.vue
@@ -3,17 +3,18 @@
 		onLaunch: function() {
 		},
 		onShow: function() {
-			
+
 		},
 		onHide: function() {
-			
+
 		}
 	}
 </script>
 
 <style lang="scss">
 	/*每个页面公共css */
-	@import "@/uni_modules/uview-ui/index.scss";
+	@import "./uni_modules/uview-ui/index.scss";
 	@import "common/demo.scss";
-	
+	@import "common/uview.reset.scss";
+
 </style>
